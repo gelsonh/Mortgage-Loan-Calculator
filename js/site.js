2032aca46b4b0e0d64a9f88877497b37ca4b850c
Gelson Hernandez <118878157+gelsonh@users.noreply.github.com>
2023-08-01 10:30:58 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,13 @@
+function displayMessage() {
+  let inputBox = document.getElementById("message");
+
+  let message = inputBox.value;
+
+  Swal.fire({
+    backdrop: false,
+    title: "App Name",
+    text: message,
+  });
+}
+
+
